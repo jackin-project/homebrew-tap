c15700990010b24fc3a3c6b0017ccb6ffdcac5dc
Alexey Zhokhov <alexey@zhokhov.com>
2026-04-18 04:33:41 +0700

    jackin@preview 0.6.0-preview.419+19d0850 (#8)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/jackin-preview.rb
+++ Formula/jackin-preview.rb
@@ -1,9 +1,9 @@
 class JackinPreview < Formula
   desc "Matrix-inspired CLI for orchestrating AI coding agents at scale"
   homepage "https://github.com/jackin-project/jackin"
-  url "https://github.com/jackin-project/jackin/archive/07ca9d85b8d97584d8d0760119bed48f17186ca2.tar.gz"
-  version "0.6.0-preview.418+07ca9d8"
-  sha256 "7c15ec3fb1c9558e8b93672922ff705eced9b9be9e4fc9711fea91c034b4ad5a"
+  url "https://github.com/jackin-project/jackin/archive/19d0850aaa2ffb8e521b568c5164769735b28f5c.tar.gz"
+  version "0.6.0-preview.419+19d0850"
+  sha256 "f3c1e8c9714657c237a1124165153f5a808fdc0f74500ceeded4e3a55b5088f2"
   license "Apache-2.0"
 
   depends_on "rust" => :build
