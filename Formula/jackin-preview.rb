602ea8f67e135b3d846f7fb0111756842078cd07
Alexey Zhokhov <alexey@zhokhov.com>
2026-04-25 07:31:00 +0700

    jackin@preview 0.6.0-preview.497+7e8470d (#86)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/jackin-preview.rb
+++ Formula/jackin-preview.rb
@@ -1,9 +1,9 @@
 class JackinPreview < Formula
   desc "CLI for orchestrating autonomous AI coding agents in isolated sandboxed environments — reproducible, scoped, and fully under your control"
   homepage "https://github.com/jackin-project/jackin"
-  url "https://github.com/jackin-project/jackin/archive/41ae0663e439e2e647a997c6c291fb33768a1ec3.tar.gz"
-  version "0.6.0-preview.496+41ae066"
-  sha256 "2c8aab11d1e98ff586cb1892779554fc5d46c239a5d4afdff500cc260246026c"
+  url "https://github.com/jackin-project/jackin/archive/7e8470d951f0d447157720dd258d850e1fc0e2a9.tar.gz"
+  version "0.6.0-preview.497+7e8470d"
+  sha256 "2ccd438ce67693489d227eae4184ae94d46fef8ad9d917393c1781010b6d620e"
   license "Apache-2.0"
 
   depends_on "rust" => :build
